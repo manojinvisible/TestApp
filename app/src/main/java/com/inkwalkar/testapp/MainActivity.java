0036d2d3c85d394f1d781e6c8dd7146e85a309f1
AHK <ayushjha6@gmail.com>
2015-04-06 02:52:24 +0545

    adding fields

--- app/src/main/java/com/inkwalkar/testapp/MainActivity.java
+++ app/src/main/java/com/inkwalkar/testapp/MainActivity.java
@@ -1,13 +1,17 @@
 package com.inkwalkar.testapp;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    public final static String EXTRA_MESSAGE = "com.inkwalkar.testapp.MainActivity Message :";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,5 +39,13 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void SendMessage(View view){
+        Intent intent = new Intent(this, SendActivity.class);
+        EditText editText = (EditText) findViewById(R.id.content);
+        String message= editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }

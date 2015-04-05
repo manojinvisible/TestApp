b10f7ee2e510ae94a548d431aa2ce6b12f3abc6b
manoj rana <manojinvisible@gmail.com>
2015-04-06 02:55:14 +0545

    changes on send activity

--- app/src/main/java/com/inkwalkar/testapp/SendActivity.java
+++ app/src/main/java/com/inkwalkar/testapp/SendActivity.java
@@ -1,16 +1,22 @@
 package com.inkwalkar.testapp;
 
+import android.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.content.Intent;
 
 
 public class SendActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         setContentView(R.layout.activity_send);
     }
 
